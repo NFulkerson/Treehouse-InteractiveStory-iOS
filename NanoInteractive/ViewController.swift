f115f1444fd57b39fa5554fe683e852e009d53d2
Nathan Fulkerson <nm.fulkerson@gmail.com>
2016-11-30 18:42:34 +0900

    Model refactor. Animate UITextField within view.

--- NanoInteractive/ViewController.swift
+++ NanoInteractive/ViewController.swift
@@ -8,26 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
+    
+    
+    enum AdventureError : Error {
+        case noName
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let story = Page(story: .TouchDown)
-        story.firstChoice = (title: "Some Title", page: Page(story: .Droid))
+        NotificationCenter.default.addObserver(self,
+            selector: #selector(ViewController.keyboardWillShow(notification:)),
+            name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(notification:)),
+            name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "adventureStartedSegue") {
-            if let pageController = segue.destination as? PageViewController {
-                pageController.page = Adventure.story
+            
+            do {
+                if let name = nameTextField.text {
+                    if name == "" {
+                        throw AdventureError.noName
+                    }
+                    
+                    if let pageController = segue.destination as? PageViewController {
+                        pageController.page = Adventure.story(name: name)
+                    }
+                }
+            } catch AdventureError.noName {
+                let alertController = UIAlertController(title: "Name Not Provided", message: "Enter a name to start the story.", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(action)
+                present(alertController, animated: true, completion: nil)
+            } catch let error {
+                fatalError("\(error)")
+            }
+            }
+    }
+    
+    
+    func keyboardWillShow(notification: Notification) {
+        if let userInfoDict = notification.userInfo, let keyboardFrameValue = userInfoDict[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardFrame = keyboardFrameValue.cgRectValue
+//            functional but jumpy
+//            textFieldBottomConstraint.constant = keyboardFrame.size.height + 10
+            UIView.animate(withDuration: 0.8) {
+                self.textFieldBottomConstraint.constant = keyboardFrame.size.height + 10
+                self.view.layoutIfNeeded()
             }
         }
+    }
+    
+    func keyboardWillHide(notification: Notification) {
+        textFieldBottomConstraint.constant = 140
+        view.layoutIfNeeded()
+    }
+    
+// Prior to iOS 9 this would be necessary
+//    deinit {
+//        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+//    }
+    
+    // MARK: - UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
